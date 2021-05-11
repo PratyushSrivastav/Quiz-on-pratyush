a0c2224044e1050be7a7c36e11ccf32c8e9d56ce
Pratyush Srivastava <76805693+PratyushSrivastav@users.noreply.github.com>
2021-05-11 20:21:46 +0530

    Added Source Code File.

---
+++ index.js
@@ -0,0 +1,61 @@
+var readlineSync=require('readline-sync');
+var chalk=require('chalk');
+
+var questionSet=[{
+  question: "Where do I live? ",
+  answer: "lucknow"
+},
+{
+  question:"do I work? ",
+  answer: "no"
+},
+{
+  question:"Where did I grew up? ",
+  answer: "bahraich"
+},
+{
+  question:"My graduation is in which field? ",
+  answer: "i.t."
+},
+{
+  question:"Do I love to spend or invest? ",
+  answer: "invest"
+}];
+
+var questionSet2=[{
+question: "What genre of articles do I read? ",
+answer: "technology"
+},
+{
+  question:"Do I love cycling or running? ",
+  answer:"cycling"
+},
+{
+  question:"Am I a non vegetarian? ",
+  answer:"yes"
+},
+{
+  question:"What is my Hobby? ",
+  answer: "photography"
+}];
+
+
+var highScore=[
+  {
+    user: "Mom and Dad",
+    score: 9
+  },
+  {
+    user: "Prateesh",
+    score: 8
+  },
+  {
+    user: "Prateek",
+    score: 7
+  },
+  {
+    user: "Sandeep",
+    score: 6
+  }
+];
+
